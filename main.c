19eddfea35f7a20d78afb14cd442de02eeccc042
Bladen Ward <bladen@bladen.dev>
2024-10-14 12:32:18 -0500

    feat: WIP initial server

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    if (argc < 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+    printf("port: %s\n", argv[1]);
+
     return 0;
 }
